33945c8e5d9d199e0cd7b46a1abfba9df81d5050
Nii Kunihiro <nii.kunihiro@gmail.com>
2020-08-04 01:13:13 +0900

    add chi router

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"path/filepath"
+
+	"github.com/go-chi/chi"
 )
 
 func main() {
 	conf := flag.String("config", "etc/go/conf.yaml", "configuration file path")
+	port := flag.Int("port", 3333, "server port")
 	flag.Parse()
 
 	file, dir := filepath.Base(*conf), filepath.Dir(*conf)
@@ -21,5 +25,10 @@ func main() {
 	defer fp.Close()
 
 	b, _ := ioutil.ReadAll(fp)
-	fmt.Println(string(b))
+
+	r := chi.NewRouter()
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write(b)
+	})
+	http.ListenAndServe(fmt.Sprintf(":%d", port), r)
 }
